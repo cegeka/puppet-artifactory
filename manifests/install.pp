3cf9014d1e9f14662ef31c854e5221ff6825cddb
Timothy Vandenbrande <timothy.vandenbrande@cegeka.com>
2018-08-08 10:17:51 +0200

    no wget jdk

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,12 @@
 #
 # This class is called from artifactory for install.
 #
+
+include profile::iac::java_jdk
+
 class artifactory::install {
   package { $::artifactory::package_name:
     ensure  => $::artifactory::package_version,
+    require => Package['jdk']
   }
 }
